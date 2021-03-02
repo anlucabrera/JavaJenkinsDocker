3b3d3fc66bc7cad2179310f2416d2768d37427d1
IYEAD <IYEAD@DESKTOP-AC416MI>
2021-03-02 15:49:44 -0600

    Registro de calificaciones para reincorporaciones

--- src/main/java/mx/edu/utxj/pye/sgi/controlador/controlEscolar/ControlEscolarMenu.java
+++ src/main/java/mx/edu/utxj/pye/sgi/controlador/controlEscolar/ControlEscolarMenu.java
@@ -96,9 +96,9 @@ public class ControlEscolarMenu implements Desarrollable, Desplegable {
         get = Faces.evaluateExpressionGet("#{seguimientoExpedienteMatriculaTitulacion.tieneAcceso}"); if(get) return true;
         get = Faces.evaluateExpressionGet("#{fechasTerminacionTitulacion.tieneAcceso}"); if(get) return true;        
         get = Faces.evaluateExpressionGet("#{registroExpedienteTitulacion.tieneAcceso}"); if(get) return true;
-        get = Faces.evaluateExpressionGet("#{registrolDelEstudiante.tieneAcceso}"); if(get) return true;
-        get = Faces.evaluateExpressionGet("#{registroDelEstudianteConsulta.tieneAcceso}"); if(get) return true;
-        get = Faces.evaluateExpressionGet("#{resultadosPlaneacionesConsulta.tieneAcceso}"); if(get) return true;
+//        get = Faces.evaluateExpressionGet("#{registrolDelEstudiante.tieneAcceso}"); if(get) return true;
+//        get = Faces.evaluateExpressionGet("#{registroDelEstudianteConsulta.tieneAcceso}"); if(get) return true;
+//        get = Faces.evaluateExpressionGet("#{resultadosPlaneacionesConsulta.tieneAcceso}"); if(get) return true;
         get = Faces.evaluateExpressionGet("#{registroFichaAdmisionSE.tieneAcceso}"); if(get) return true;
         get = Faces.evaluateExpressionGet("#{asignacionRolesEstadiaDirector.tieneAcceso}"); if(get) return true;
         get = Faces.evaluateExpressionGet("#{asignacionEstudiantesEstadiaDocente.tieneAcceso}"); if(get) return true;
